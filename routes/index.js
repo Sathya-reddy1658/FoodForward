378703b2a008459ec6eda6383ed58c0210e73f95
GUNUPURU SURAJ <146669517+suraj-9849@users.noreply.github.com>
2024-07-04 11:55:28 +0530

    Added the Functionality of the Stripe Payment gateway is setUpped

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 var express = require('express');
 var router = express.Router();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY) 
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'FoodForward', loggedIn: req.session.loggedIn });
@@ -12,9 +14,46 @@ router.get('/about', function(req, res, next) {
 router.get('/Charity', function(req, res, next) {
   res.render('Charity', { title: 'Charity' }); 
 });
-router.get('/stripe', function(req, res, next) {
-  res.render('stripe', { title: 'Stripe' }); 
+
+// Stripe integration 
+router.post('/stripe', async function(req, res, next) {
+  try {
+    const donationAmount = req.body.amount || 10;
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'], 
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Food Forward Donation',
+              description: 'Donation from Poor People',
+            },
+            unit_amount: donationAmount * 100, 
+          },
+          quantity: 1,
+        }
+      ],
+      mode: 'payment',
+      success_url: `${process.env.BASE_URL}/complete`, 
+      cancel_url: `${process.env.BASE_URL}/cancel`,
+    });
+res.redirect(session.url);
+  } catch (error) {
+    console.error("Error creating Stripe session:", error); 
+    res.status(500).send("Internal Server Error");
+  }
 });
+
+
+router.get("/complete", function(req, res) {
+res.render("complete");
+}  
+)
+router.get("/cancel", function(req, res) {
+res.render("cancel")
+})
 
 router.get('/feeding', function(req, res, next) {
   res.render('awards', { title: 'Feeding' }); 
